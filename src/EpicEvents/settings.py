15e8417629d849be5c7e0ca6b583408035e1037a
LN_SniperKing <luke.c@laposte.net>
2022-02-05 15:37:48 +0100

    Replace "eeapi" by "accounts"

--- src/EpicEvents/settings.py
+++ src/EpicEvents/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'eeapi'
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Ovewrite Django User by this custom class
+AUTH_USER_MODEL = "accounts.User"
