7e75402c120e2d03e46061eb7d7fa942df7a7f7f
LN_SniperKing <luke.c@laposte.net>
2022-02-10 17:51:26 +0100

    Remove view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,17 +1,2 @@
 from django.shortcuts import render
 
-from rest_framework.viewsets import ModelViewSet
-from .models import User
-from .serializers import UserSerializer
-
-
-class UsersViewset(ModelViewSet):
-    """ Comments list"""
-    serializer_class = UserSerializer
-    # permission_classes = [IsAuthenticated]
-    http_method_names = ['get']
-
-    # lookup_field = 'comment_id'  # Use to show detail page
-    #
-    def get_queryset(self):
-        return User.objects.all()
