6b74183cd9c64001b4cf9170a795e0013e440f3a
LN_SniperKing <luke.c@laposte.net>
2022-02-18 14:40:28 +0100

    Remove migrations

--- src/clients/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
-from .models import Client
-from .serializers import ClientSerializer
-from rest_framework.response import Response
-from rest_framework import status
-
