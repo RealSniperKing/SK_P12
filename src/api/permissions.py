19aa95c6dcac98c4c94a823340f25f719c4cdff9
LN_SniperKing <luke.c@laposte.net>
2022-02-24 09:59:43 +0100

    Add all permissions

--- src/api/permissions.py
+++ src/api/permissions.py
@@ -20,14 +20,10 @@ class D7896DjangoModelPermissions(DjangoModelPermissions):
     }
 
 
-class CustomDjangoModelPermissions(DjangoModelPermissions):
-    # def __init__(self, parameters):
-    #     self.parameters = parameters
-    #     # self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
-
-    # perms_map = {}
+# ZERO ACTION
+class ZeroDjangoModelPermissions(DjangoModelPermissions):
     perms_map = {
-        # 'GET': ['%(app_label)s.view_%(model_name)s'],
+        #'GET': ['%(app_label)s.view_%(model_name)s'],
         'OPTIONS': [],
         'HEAD': [],
         #'POST': ['%(app_label)s.add_%(model_name)s'],
@@ -36,29 +32,81 @@ class CustomDjangoModelPermissions(DjangoModelPermissions):
         #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
 
-
-class ReadDjangoModelPermissions(DjangoModelPermissions):
+# ONE ACTION
+class AddDjangoModelPermissions(DjangoModelPermissions):
     perms_map = {
-        'GET': ['%(app_label)s.view_%(model_name)s'],
-        'OPTIONS': [],
-        'HEAD': [],
-        #'POST': ['%(app_label)s.add_%(model_name)s'],
-        #'PUT': ['%(app_label)s.change_%(model_name)s'],
-        #'PATCH': ['%(app_label)s.change_%(model_name)s'],
-        #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
-    }
-
-
-class PostDjangoModelPermissions(DjangoModelPermissions):
-    perms_map = {
-        'GET': ['%(app_label)s.view_%(model_name)s'],
+        #'GET': ['%(app_label)s.view_%(model_name)s'],
         'OPTIONS': [],
         'HEAD': [],
         'POST': ['%(app_label)s.add_%(model_name)s'],
+        #'PUT': ['%(app_label)s.change_%(model_name)s'],
+        #'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
 
 
-class PutPatchDjangoModelPermissions(DjangoModelPermissions):
+class ChangeDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        #'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        #'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class DeleteDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        #'GET': ['%(app_label)s.view_%(model_name)s'],
+        #'OPTIONS': [],
+        #'HEAD': [],
+        #'POST': ['%(app_label)s.add_%(model_name)s'],
+        #'PUT': ['%(app_label)s.change_%(model_name)s'],
+        #'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class ViewDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        #'POST': ['%(app_label)s.add_%(model_name)s'],
+        #'PUT': ['%(app_label)s.change_%(model_name)s'],
+        #'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+# TWO ACTIONS
+class AddChangeDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        #'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class AddDeleteDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        #'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        #'PUT': ['%(app_label)s.change_%(model_name)s'],
+        #'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class AddViewDjangoModelPermissions(DjangoModelPermissions):
     perms_map = {
         'GET': ['%(app_label)s.view_%(model_name)s'],
         'OPTIONS': [],
@@ -66,10 +114,97 @@ class PutPatchDjangoModelPermissions(DjangoModelPermissions):
         'POST': ['%(app_label)s.add_%(model_name)s'],
         'PUT': ['%(app_label)s.change_%(model_name)s'],
         'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
 
 
-class DeleteDjangoModelPermissions(DjangoModelPermissions):
+class ChangeDeleteDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class ChangeViewDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        #'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class DeleteViewDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        #'POST': ['%(app_label)s.add_%(model_name)s'],
+        #'PUT': ['%(app_label)s.change_%(model_name)s'],
+        #'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+# THREE ACTIONS
+class AddChangeDeleteDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        #'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class AddChangeViewDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        #'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class AddDeleteViewDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        #'PUT': ['%(app_label)s.change_%(model_name)s'],
+        #'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+# FOUR ACTIONS
+class ChangeDeleteViewDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        #'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
+class AddChangeDeleteViewDjangoModelPermissions(DjangoModelPermissions):
     perms_map = {
         'GET': ['%(app_label)s.view_%(model_name)s'],
         'OPTIONS': [],
