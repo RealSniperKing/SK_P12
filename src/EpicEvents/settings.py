1eaca4d68fe07328edb0756a17be11368a2eb82f
LN_SniperKing <luke.c@laposte.net>
2022-02-25 16:32:08 +0100

    Edit authentification

--- src/EpicEvents/settings.py
+++ src/EpicEvents/settings.py
@@ -164,6 +164,7 @@ REST_FRAMEWORK = {
         #'rest_framework_simplejwt.authentication.JWTAuthentication',
         #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
