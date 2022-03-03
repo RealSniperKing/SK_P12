b78a11e507ad7b38e7b11a4ed4d89f5d4dfd6e43
LN_SniperKing <luke.c@laposte.net>
2022-03-03 11:35:50 +0100

    Add Client, Contract and Event models to admin interface

--- src/crm/admin.py
+++ src/crm/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from .models import Client, Contract, Event
 # Register your models here.
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'client_id')  # fields to display in the listing
+    empty_value_display = '-empty-'        # display value when empty
+    list_filter = ()      # enable results filtering
+    list_per_page = 25                     # number of items per page
+    ordering = ['-updating_time', 'email']       # Default results ordering
+
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('title', 'contract_id')  # fields to display in the listing
+    empty_value_display = '-empty-'        # display value when empty
+    list_filter = ()      # enable results filtering
+    list_per_page = 25                     # number of items per page
+    ordering = ['-updating_time', 'title']       # Default results ordering
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event_id')  # fields to display in the listing
+    empty_value_display = '-empty-'        # display value when empty
+    list_filter = ()      # enable results filtering
+    list_per_page = 25                     # number of items per page
+    ordering = ['-updating_time', 'name']       # Default results ordering
+
+
+# and register it
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Contract, ContractAdmin)
+admin.site.register(Event, EventAdmin)
