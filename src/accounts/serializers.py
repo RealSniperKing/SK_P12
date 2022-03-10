5e17a57a59b968b0927bb59e21a9cb70dc4cd03e
Luc <luke.c@laposte.net>
2022-03-10 21:12:53 +0100

    Edit groups and role

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from .models import User
+from django.contrib.auth.models import Group
 
 
 # CONNECTION
@@ -46,6 +47,12 @@ class UserSmallSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     confirm_password = serializers.CharField(style={'input_type': 'password'}, allow_blank=False, write_only=True)
+    #client_manager = serializers.SlugRelatedField(slug_field='email', allow_null=True, queryset=User.objects.all())
+    # groups = serializers.SlugRelatedField(slug_field='name', allow_null=True, queryset=Group.objects.all())
+    # groups = serializers.SlugRelatedField(slug_field='name', allow_null=True, queryset=Group.objects.all())
+    # groups = serializers.SlugRelatedField(many=True, slug_field='name', allow_null=True, queryset=Group.objects.all())
+    groups = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
+    # group_name = serializers.SlugRelatedField(slug_field='name', write_only=True, allow_null=True, queryset=Group.objects.all())
 
     class Meta:
         model = User
@@ -54,24 +61,40 @@ class UserSerializer(serializers.ModelSerializer):
                         'password': {'write_only': True,
                                      'style': {'input_type': 'password'}},
                         'lookup_field': 'user_id'}
+    print("-------+++++")
 
     def validate(self, data):
+        # Passwords matching ?
         if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("passwords not match")
-        # return data
+
+        # Check if the user exist
         email = data["email"]
         users = User.objects.filter(email=email)
-        if not users:
-            return data
-        else:
+        if users.exists():
             raise serializers.ValidationError('This user already exist')
+
+        # Check if the group_name exist
+        # role = data["role"]
+        # if role:
+        #     group_ob = Group.objects.filter(name=role)
+        #     if not group_ob:
+        #         raise serializers.ValidationError('Invalid group name')
+
+        # group_ob = Group.objects.filter(name=group_name)
+        # if not group_ob:
+        #     raise serializers.ValidationError('Instance not found')
+
+        return data
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
+    groups = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
+
     class Meta:
         model = User
         fields = ['email', 'user_id', 'role', 'groups']
-        # extra_kwargs = {'groups': {'read_only': True}}
+        extra_kwargs = {'groups': {'read_only': True}}
 
     def validate(self, data):
         return data
