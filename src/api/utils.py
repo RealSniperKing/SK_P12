40ea80640ed8c18fcb851a6ae6676e46342da8b1
Luc <luke.c@laposte.net>
2022-03-10 21:16:35 +0100

    Custom handler 404 not found

---
+++ src/api/utils.py
@@ -0,0 +1,22 @@
+from rest_framework.views import exception_handler
+from rest_framework.exceptions import NotFound
+
+
+def custom_exception_handler(exc, context):
+    """
+    Custom message '404 not found'
+    REST_FRAMEWORK settings : 'EXCEPTION_HANDLER': 'api.utils.custom_exception_handler'
+    """
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+
+    # Now add the HTTP status code to the response.
+    if response is not None and response.status_code == 404:
+        response.data = {
+            "message": "Instance not found.",
+            "success": False
+        }
+
+    # raise NotFound(detail="Error 404, page not found", code=404)
+    return response
