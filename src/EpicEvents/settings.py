16752dc859920f30617018ccbebe86219fe4961c
Luc <luke.c@laposte.net>
2022-03-18 16:19:50 +0100

    Add pagination

--- src/EpicEvents/settings.py
+++ src/EpicEvents/settings.py
@@ -163,6 +163,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
         "rest_framework.permissions.DjangoModelPermissions",
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
