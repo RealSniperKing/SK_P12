c854965634ca50a1f5ef5d580aafe49f6689bf7e
Luc <luke.c@laposte.net>
2022-03-25 21:21:38 +0100

    Fix permissions

--- src/api/permissions.py
+++ src/api/permissions.py
@@ -3,6 +3,8 @@ import copy
 from rest_framework import permissions
 from rest_framework.permissions import DjangoModelPermissions
 from accounts.models import User, ManagementGroupName
+from django.contrib.auth.models import Group
+
 
 class D7896DjangoModelPermissions(DjangoModelPermissions):
     # def __init__(self):
@@ -24,22 +26,43 @@ class IsManagerOrAdminManager(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
 
+    def has_permission(self, request, view):
+        """list permissions"""
+        if request.method in view.allowed_methods:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        """detail permissions"""
+        print("///////////////////////")
         model_name = obj._meta.model.__name__
+        print("model_name = ", model_name)
         user = request.user
+        if user is None:
+            return False
 
         # Admin permission
         if user.is_admin:
             return True
 
-        if user in User.objects.filter(groups__name=ManagementGroupName.objects.first().name):
+        # Is management team
+        management_group_name = ManagementGroupName.objects.all()
+        management_group = None
+        if len(management_group_name) == 1:
+            management_group_list = Group.objects.filter(name=management_group_name.first().name)
+            if not management_group_list:
+                management_group = None
+            management_group = management_group_list.first()
+        group_all = []
+        group_all.extend(user.groups.all())
+        print("management_group = ", management_group)
+        print("group_all = ", group_all)
+        if management_group in group_all:
             return True
 
         # Client permission
-        if model_name == "Client":
+        if model_name == "Customer":
             return obj.client_manager == user
 
         # Contract permission
@@ -49,6 +72,8 @@ class IsManagerOrAdminManager(permissions.BasePermission):
         # Event permission
         if model_name == "Event":
             return obj.event_manager == user
+
+        return False
 
 
 
