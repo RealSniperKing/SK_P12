38134e998e1431dd798d59fc704da443937816b5
Luc <luke.c@laposte.net>
2022-03-25 21:23:40 +0100

    Add tests

--- src/api/tests/test_management_team.py
+++ src/api/tests/test_management_team.py
@@ -19,17 +19,28 @@ def test_views_permissions_true():
     group_name = "Equipe de gestion"
     management_permissions = {"group_name": group_name, "permissions_list": [
         {"model": User, "permissions": ["view", "add", "change", "delete"]},
-        {"model": Contract, "permissions": ["view", "add", "change", "delete"]},
         {"model": Customer, "permissions": ["view", "add", "change", "delete"]},
+        {"model": Contract, "permissions": ["view", "add", "change", "delete"]},
         {"model": Event, "permissions": ["view", "add", "change", "delete"]}
     ]}
     api_test.assign_permissions_to_user(management_permissions, group_name)
+    api_test.set_management_group_name(group_name)
 
+    # USERS URL
     crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
     api_test.launch_user_crud_actions("users", crud_actions)
+
+    # CLIENTS URL
+    crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
     api_test.launch_customer_crud_actions("clients", crud_actions)
-    # api_test.launch_crud_actions("contracts", crud_actions)
-    # api_test.launch_crud_actions("events", crud_actions)
+
+    # CONTRACT URL
+    crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
+    api_test.launch_contract_crud_actions("contracts", crud_actions)
+
+    # EVENT URL
+    crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
+    api_test.launch_event_crud_actions("events", crud_actions)
 
 
 @pytest.mark.django_db(transaction=True)
