ad8136e06f42cb67bbc1f087f85f7dad0d22d8fa
Luc <luke.c@laposte.net>
2022-03-25 21:22:59 +0100

    Add contract crud actions

--- src/api/tests/controllers.py
+++ src/api/tests/controllers.py
@@ -1,4 +1,5 @@
 from .models import Api
+from accounts.models import ManagementGroupName
 
 
 class ApiTest:
@@ -18,6 +19,18 @@ class ApiTest:
 
         # Logout
         self.api.signout()
+
+    def set_management_group_name(self, group_name):
+        management_group_name = ManagementGroupName.objects.all()
+
+        if len(management_group_name) == 0:
+            management_team = ManagementGroupName.objects.create(name=group_name)
+            management_team.save()
+        else:
+            management_team = management_group_name.first()
+            management_team.name = group_name
+            management_team.save()
+        # print("ManagementGroupName ==== ", ManagementGroupName.objects.all())
 
     def launch_user_crud_actions(self, view_name, crud_actions):
         """Test CRUD operations : create, read, update, delete"""
@@ -56,7 +69,7 @@ class ApiTest:
         # GET
         self.api.view_get(f"api:{view_name}-list", crud_actions["read"])
 
-        # # POST
+        # POST
         compagny_name = "company_1"
         compagny_email = "company@mail.fr"
         data = {"company_name": compagny_name, "email": compagny_email}
@@ -70,8 +83,46 @@ class ApiTest:
         data = {"email": "company_edit@mail.fr"}
         self.api.view_put(f"api:{view_name}-detail", crud_actions["update"], data=data, client_id=client_id)
 
-        # # DELETE
-        # self.api.view_delete(f"api:{view_name}-detail", crud_actions["delete"], user_id=user_id)
+        # DELETE
+        self.api.view_delete(f"api:{view_name}-detail", crud_actions["delete"], client_id=client_id)
 
         # Logout
         self.api.signout()
+
+    def launch_contract_crud_actions(self, view_name, crud_actions):
+        """Test CRUD operations : create, read, update, delete"""
+        # Connection
+        self.api.signin()
+
+        # GET
+        self.api.view_get(f"api:{view_name}-list", crud_actions["read"])
+
+        # POST
+        title = "contract1"
+        data = {"title": title, "client": "", "contract_manager": ""}
+        json_content = self.api.view_post(f"api:{view_name}-list", crud_actions["create"], data=data)
+        contract_id = json_content.get('contract_id', "0")
+
+        # GET DETAIL
+        self.api.view_get(f"api:{view_name}-detail", crud_actions["read"], contract_id=contract_id)
+
+        # PUT
+        data = {"title": "contract1_edit"}
+        self.api.view_put(f"api:{view_name}-detail", crud_actions["update"], data=data, contract_id=contract_id)
+
+        # DELETE
+        self.api.view_delete(f"api:{view_name}-detail", crud_actions["delete"], contract_id=contract_id)
+
+        # Logout
+        self.api.signout()
+
+    def launch_event_crud_actions(self, view_name, crud_actions):
+        """Test CRUD operations : create, read, update, delete"""
+        # Connection
+        self.api.signin()
+
+        # POST
+        name = "Event_1"
+        data = {"name": name, "contract": "", "event_manager": ""}
+        json_content = self.api.view_post(f"api:{view_name}-list", crud_actions["create"], data=data)
+        contract_id = json_content.get('event_id', "0")
