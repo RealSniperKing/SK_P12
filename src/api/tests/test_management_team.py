3e1d0bc3b5a9b4200a4a99874596a897c5393e9f
Luc <luke.c@laposte.net>
2022-03-31 22:31:15 +0200

    Edit parameters

--- src/api/tests/test_management_team.py
+++ src/api/tests/test_management_team.py
@@ -24,22 +24,22 @@ def test_views_permissions_true():
         {"model": Event, "permissions": ["view", "add", "change", "delete"]}
     ]}
     api_test.assign_permissions_to_user(management_permissions, group_name)
-    api_test.set_management_group_name(group_name)
+    api_test.set_management_group_name("Equipe de gestion")
 
     # USERS URL
-    crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
+    crud_actions = {"create": 200, "read-list": 200, "read-detail": 200, "update": 200, "delete": 200}
     api_test.launch_user_crud_actions("users", crud_actions)
 
     # CLIENTS URL
-    crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
+    crud_actions = {"create": 200, "read-list": 200, "read-detail": 200, "update": 200, "delete": 200}
     api_test.launch_customer_crud_actions("clients", crud_actions)
 
     # CONTRACT URL
-    crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
+    crud_actions = {"create": 200, "read-list": 200, "read-detail": 200, "update": 200, "delete": 200}
     api_test.launch_contract_crud_actions("contracts", crud_actions)
 
     # EVENT URL
-    crud_actions = {"create": 200, "read": 200, "update": 200, "delete": 200}
+    crud_actions = {"create": 200, "read-list": 200, "read-detail": 200, "update": 200, "delete": 200}
     api_test.launch_event_crud_actions("events", crud_actions)
 
 
@@ -57,8 +57,8 @@ def __test_views_permissions_false():
     ]}
     api_test.assign_permissions_to_user(management_permissions, group_name)
 
-    crud_actions = {"create": 405, "read": 405, "update": 405, "delete": 405}
+    crud_actions = {"create": 405, "read-list": 405, "read-detail": 405, "update": 405, "delete": 405}
     api_test.launch_user_crud_actions("users", crud_actions)
-    # api_test.launch_crud_actions("clients", crud_actions)
-    # api_test.launch_crud_actions("contracts", crud_actions)
-    # api_test.launch_crud_actions("events", crud_actions)
+    api_test.launch_crud_actions("clients", crud_actions)
+    api_test.launch_crud_actions("contracts", crud_actions)
+    api_test.launch_crud_actions("events", crud_actions)
